12fd333f1a321c8cf28ba097d82f9755ccf4ae6f
Working Tech <k2785233247@gmail.com>
2025-07-21 18:20:24 +0800

    修好了奇怪的问题, 但是路程异常的曲折, fastjson json名不对Gun.java的成员变量名, fastjson2背大锅, 不是我的错, 是fastjson2的错 :(

--- Maven_Project/src/main/java/com/tekon/App.java
+++ Maven_Project/src/main/java/com/tekon/App.java
@@ -3,8 +3,12 @@ package com.tekon;
 // 本地其他类
 import com.tekon.my_class.Gun;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 // Java内置类
-import java.nio.file.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 // dependencies中引入的其他第三方库 (json)
@@ -25,16 +29,29 @@ public class App {
         System.out.println(System.getProperty("user.dir"));
         String working_dir = System.getProperty("user.dir");
         
-        Path shotgun_path = Paths.get(working_dir,"Maven_Project/src/main/java/com/tekon/Guns_json",shotgun.get_gun_name()+".json"); // 生成'目标文件', 下面进行数据写入
+        // 突然发现了一个严重的问题, Files在java 8 下并没有writeString()方法, 这下不得不用BufferedWriter了...
+        File shotgun_path = new File(working_dir + "/Maven_Project/src/main/java/com/tekon/Guns_json/" +shotgun.get_gun_name()+".json"); // 生成'目标文件', 下面进行数据写入
         // System.out.println(Files.exists(shotgun_path));
-        Files.write(shotgun_path, json_byte, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+        BufferedWriter json_writer = new BufferedWriter(new FileWriter(shotgun_path));
+        json_writer.write(json_text); // 使用BufferedWriter对json 字符串数据进行写入
+        json_writer.flush();
+        json_writer.close();
+    
 
         System.out.println();
         // 现在json文件已经成功创建, 我们要读该json文件的'内容', 将读到的'内容'放在JSON.parseObject中解析;
-        if(Files.exists(shotgun_path)){
-            System.out.println("成功存储了 " + shotgun_path.getFileName() + ", 正在对其进行读取...");
-            byte[] readed_byte = Files.readAllBytes(shotgun_path);
-            JSONObject shotgun_json = JSON.parseObject(null);
+        if(shotgun_path.exists()){
+            System.out.println("成功存储了 " + shotgun_path.getName() + ", 正在对其进行读取...");
+            BufferedReader json_reader = new BufferedReader(new FileReader(shotgun_path));
+            StringBuilder char_arr = new StringBuilder(5);
+            char[] buffer_temp = new char[1024];
+            int value_examined;
+            while((value_examined = json_reader.read(buffer_temp)) != -1){
+                char_arr.append(buffer_temp, 0, value_examined);
+            }
+            String json_string = char_arr.toString();
+            System.out.println("json_string的内容为: " + json_string);
+            Gun imported_gun = JSON.parseObject(json_string, Gun.class);
             // Gun imported_gun = JSON.parseArray(readed_json, Gun.class); // fastjson从byte[] / String中读取数据, 转回对应的Java对象 (要把对应的类.class给传进去)
             System.out.println("导入的枪名为: " + imported_gun.get_gun_name() + ", 弹药类型为: " + imported_gun.get_ammo_type());
             try {
@@ -43,7 +60,7 @@ public class App {
                 System.out.println("尝试读取的枪的id为null...");
             }
         }else{
-            System.out.println("未能成功存储 " + shotgun_path.getFileName() + ", 程序已终止运行...");
+            System.out.println("未能成功存储 " + shotgun_path.getName() + ", 程序已终止运行...");
         }
         
         // Gun imported_shotgun =JSON.parseObject()
--- Maven_Project/src/main/java/com/tekon/my_class/Gun.java
+++ Maven_Project/src/main/java/com/tekon/my_class/Gun.java
@@ -3,12 +3,19 @@ package com.tekon.my_class; // 在maven创建的项目中, package的声明从'c
 import java.lang.Math;
 import java.util.ArrayList;
 
+import com.alibaba.fastjson2.annotation.JSONField;
+import java.io.Serializable;
+
 // import Fun_Programs.Russian_Roulette.Shell;
 
-public class Gun {
+public class Gun implements Serializable{
 
+  // 不知道怎么搞的, fastjson2里面存的成员变量名前面会'多一个 _ ', 导致和这里定义的成员变量不一致, 导致拿不到反序列化对象的数据...
+  @JSONField(name = "_gun_name")
   private String gun_name;
+  @JSONField(name = "_ammo_type")
   private String ammo_type;
+  @JSONField(name = "_gun_id")
   private transient int gun_id;
 
   public Gun(String gun_name, String ammo_type, int gun_id){
