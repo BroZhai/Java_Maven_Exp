3aaba00b6193016112ee401e04a9f0899600abd5
Working Tech <k2785233247@gmail.com>
2025-07-22 09:36:10 +0800

    准备更换为Gson库, 同时vscode的cmd环境变量突然抽风了, 成功修了回来

--- Maven_Project/src/main/java/com/tekon/App.java
+++ Maven_Project/src/main/java/com/tekon/App.java
@@ -12,7 +12,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 // dependencies中引入的其他第三方库 (json)
-import com.alibaba.fastjson2.*; // 通过修改pom.xml导入的第三方json库fastjson2 (一般第三方的'package的声明'可以在对应的说明文档使用例中找到)
+// 通过修改pom.xml导入的第三方json库Gson (一般第三方的'package的声明'对应pom.xml中的<groupId>)
 
 // 在maven项目中导入其他类时, 统一都是从'com'下开始, 基本上代码的开发就在src自己'声明的域名'下 (package声明同理) 
 public class App {
@@ -22,9 +22,7 @@ public class App {
 
         // 接下来我们用fastjson对Gun对象进行序列化 (参考: https://github.com/alibaba/fastjson2)
         // Java对象转json (这里fastjson提供了toString 和 tobyte 两种类型转换方法)
-        String json_text = JSON.toJSONString(shotgun);
-        byte[] json_byte = JSON.toJSONBytes(shotgun);
-        System.out.println("转换为的json内容为: " + json_text);
+
 
         System.out.println(System.getProperty("user.dir"));
         String working_dir = System.getProperty("user.dir");
@@ -32,36 +30,15 @@ public class App {
         // 突然发现了一个严重的问题, Files在java 8 下并没有writeString()方法, 这下不得不用BufferedWriter了...
         File shotgun_path = new File(working_dir + "/Maven_Project/src/main/java/com/tekon/Guns_json/" +shotgun.get_gun_name()+".json"); // 生成'目标文件', 下面进行数据写入
         // System.out.println(Files.exists(shotgun_path));
-        BufferedWriter json_writer = new BufferedWriter(new FileWriter(shotgun_path));
-        json_writer.write(json_text); // 使用BufferedWriter对json 字符串数据进行写入
-        json_writer.flush();
-        json_writer.close();
+
+        // BufferedWriter json_writer = new BufferedWriter(new FileWriter(shotgun_path));
+        // json_writer.write(json_text); // 使用BufferedWriter对json 字符串数据进行写入
+        // json_writer.flush();
+        // json_writer.close();
     
 
         System.out.println();
         // 现在json文件已经成功创建, 我们要读该json文件的'内容', 将读到的'内容'放在JSON.parseObject中解析;
-        if(shotgun_path.exists()){
-            System.out.println("成功存储了 " + shotgun_path.getName() + ", 正在对其进行读取...");
-            BufferedReader json_reader = new BufferedReader(new FileReader(shotgun_path));
-            StringBuilder char_arr = new StringBuilder(5);
-            char[] buffer_temp = new char[1024];
-            int value_examined;
-            while((value_examined = json_reader.read(buffer_temp)) != -1){
-                char_arr.append(buffer_temp, 0, value_examined);
-            }
-            String json_string = char_arr.toString();
-            System.out.println("json_string的内容为: " + json_string);
-            Gun imported_gun = JSON.parseObject(json_string, Gun.class);
-            // Gun imported_gun = JSON.parseArray(readed_json, Gun.class); // fastjson从byte[] / String中读取数据, 转回对应的Java对象 (要把对应的类.class给传进去)
-            System.out.println("导入的枪名为: " + imported_gun.get_gun_name() + ", 弹药类型为: " + imported_gun.get_ammo_type());
-            try {
-                System.out.println("导入的枪的id为: " + imported_gun.get_gun_id());
-            } catch (Exception e) {
-                System.out.println("尝试读取的枪的id为null...");
-            }
-        }else{
-            System.out.println("未能成功存储 " + shotgun_path.getName() + ", 程序已终止运行...");
-        }
         
         // Gun imported_shotgun =JSON.parseObject()
     }
--- Maven_Project/src/main/java/com/tekon/my_class/Gun.java
+++ Maven_Project/src/main/java/com/tekon/my_class/Gun.java
@@ -11,12 +11,14 @@ import java.io.Serializable;
 public class Gun implements Serializable{
 
   // 不知道怎么搞的, fastjson2里面存的成员变量名前面会'多一个 _ ', 导致和这里定义的成员变量不一致, 导致拿不到反序列化对象的数据...
-  @JSONField(name = "_gun_name")
+  // 根据小道消息, 我们准备换成Gson重新进行实验
+  
+  // @JSONField(name = "_gun_name")
   private String gun_name;
-  @JSONField(name = "_ammo_type")
+  // @JSONField(name = "_ammo_type")
   private String ammo_type;
-  @JSONField(name = "_gun_id")
-  private transient int gun_id;
+  // @JSONField(name = "_gun_id")
+  private transient int gun_id; // 在使用第三方序列化库时, transient关键字可能会失效
 
   public Gun(String gun_name, String ammo_type, int gun_id){
     this.gun_name = gun_name;
@@ -33,7 +35,7 @@ public class Gun implements Serializable{
     return this.ammo_type;
   }
 
-  public int get_gun_id(){ // 在序列化对象后会失效, 因为gun_id 为transient
+  public int get_gun_id(){ // 用第三方序列化库的时候, 该项可能可以正常获取
     return this.gun_id;
   }
 
