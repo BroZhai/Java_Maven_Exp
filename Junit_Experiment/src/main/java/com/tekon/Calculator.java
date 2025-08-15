2b286830e3946e945fd5e2bf978d10f90fccee54
Working Tech <k2785233247@gmail.com>
2025-08-15 11:48:29 +0800

    准备实践Junit的注解类时踩了一个究极大坑, @BeforeAll必须要有一个@Test才能跑起来, 同时每个注解类对应的方法要严格遵循对应的'修饰样式'

--- Junit_Experiment/src/main/java/com/tekon/Calculator.java
+++ Junit_Experiment/src/main/java/com/tekon/Calculator.java
@@ -3,8 +3,24 @@ package com.tekon;
 public class Calculator
 {
     // 我们来实践一下Junit的简单测试, 这里先正常实现一下计算器的运作逻辑
+    static int add(int a, int b){
+        return a+b;
+    }
+    static int sub(int a, int b){
+        return a-b;
+    }
+    static int multiply(int a, int b){
+        return a*b;
+    }
+
+    static double devide(int a, int b){
+        double da = (double) a;
+        return da/b;
+    }
+
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
--- Junit_Experiment/src/test/java/com/tekon/CalculatorTest.java
+++ Junit_Experiment/src/test/java/com/tekon/CalculatorTest.java
@@ -1,38 +1,18 @@
 package com.tekon;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.*; // 引入Junit 5 的所有'注解'
 
-/**
- * Unit test for simple App.
- */
-public class CalculatorTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public CalculatorTest( String testName )
-    {
-        super( testName );
+class CalculatorTest{
+    // 大坑: BeforeAll要必须至少有一个Test才能跑起来!
+    @BeforeAll
+    public static void init(){
+        System.out.println("正在启动计算机测试程序, 请稍后...");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( CalculatorTest.class );
+    @Test
+    public void add_test(){
+        System.out.println("加法测试");
+        System.out.println("Go fuck yourself");
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    
 }
