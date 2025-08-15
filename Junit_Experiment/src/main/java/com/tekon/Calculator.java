a2aabfdd65cff6f84ee81c055fe36c221b0833ef
Working Tech <k2785233247@gmail.com>
2025-08-15 10:59:33 +0800

    创建了新的工程, 准备重点实践一下Junit

---
+++ Junit_Experiment/src/main/java/com/tekon/Calculator.java
@@ -0,0 +1,10 @@
+package com.tekon;
+
+public class Calculator
+{
+    // 我们来实践一下Junit的简单测试, 这里先正常实现一下计算器的运作逻辑
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
