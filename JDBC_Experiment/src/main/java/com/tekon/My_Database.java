f2462648fe9db250069366c0ba3c76265507bf0b
Working Tech <k2785233247@gmail.com>
2025-08-16 10:54:40 +0800

    新建一个工程, 准备研究一下JDBC怎么用

---
+++ JDBC_Experiment/src/main/java/com/tekon/My_Database.java
@@ -0,0 +1,9 @@
+package com.tekon;
+
+public class My_Database 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- JDBC_Experiment/src/test/java/com/tekon/My_Database_Test.java
+++ JDBC_Experiment/src/test/java/com/tekon/My_Database_Test.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class My_Database_Test 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public My_Database_Test( String testName )
     {
         super( testName );
     }
