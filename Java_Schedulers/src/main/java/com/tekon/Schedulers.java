c7cda5ee7a497bd3b500270857fb7c89f0c02a5d
Working Tech <k2785233247@gmail.com>
2026-05-14 15:16:51 +0800

    项目诈尸更新, 本次来详细研究下Java中的不同单机任务调度 ouo

---
+++ Java_Schedulers/src/main/java/com/tekon/Schedulers.java
@@ -0,0 +1,13 @@
+package com.tekon;
+
+/**
+ * Hello world!
+ *
+ */
+public class Schedulers 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
